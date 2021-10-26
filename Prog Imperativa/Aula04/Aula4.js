9e0bebd1087fae58bdbd688d5d847b142eb84d19
thabsc <52264465+thabsc@users.noreply.github.com>
2021-10-26 20:48:04 -0300

    Add files via upload

---
+++ Prog Imperativa/Aula04/Aula4.js
@@ -0,0 +1,14 @@
+//Sala 5 - Thabata Oliveira, Victor Leao, Rodrigo Costa, Andre Pedreschi, Maryanne Lima, Matheus Abreu
+
+var nome = "Aline"
+var idade = 33
+var peso = 63.7
+var altura = 1.53
+var plano = 0
+
+
+var IMC = Math.round(peso / Math.pow(altura, 2));
+
+console.log(`${nome} tem ${idade} anos e seu IMC é de ${IMC}`);
+
+
